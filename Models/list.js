973ca71939fec2c8f0042e8c3a42db19cafb85cd
RAFAEL_DAVID_ROMERO@HOTMAIL.COM <rafael_david_romero@ies21.onmicrosoft.com>
2021-04-12 11:47:47 -0300

    To Do List Back End

--- Models/list.js
+++ Models/list.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 var listSchema = new Schema({
     termino: { type: String, required: [true, 'El campo no puede recibir datos vacios'] },
-
+    complete: { type: Boolean, require: false }
 }, { collection: 'list' });
 
 module.exports = mongoose.model('List', listSchema);
--- Routes/list.js
+++ Routes/list.js
@@ -11,9 +11,10 @@ app.use(bodyParser.json()); // body en formato json
 app.use(bodyParser.urlencoded({ extended: false })); //body formulario    
 
 
+
 // Importar Schema de List
 var List = require("../models/list");
-var lst = [];
+
 
 //===========================================
 //        Obtener toda la lista
@@ -32,12 +33,13 @@ app.get("/", (req, res, next) => {
             });
 
         }
-        List.count({}, (error, conteo) => {
 
-            lst = lista;
+        res.status(200).json({
+            ok: true,
+            resultado: lista,
+            mensaje: 'Operacion realizada con exito'
+        })
 
-            res.render('index.ejs', { todo: lst });
-        });
 
     });
 
@@ -48,12 +50,13 @@ app.get("/", (req, res, next) => {
 //        Crear una nueva lista
 //===========================================
 app.post('/', (req, res) => {
-    var termino = req.body.termino;
+        var termino = req.body.termino;
 
-    var newList = new List({
-        termino: termino
-    })
-    if (termino != "") {
+        var newList = new List({
+            termino: termino,
+            complete: false
+        })
+
 
         newList.save((error, newTermino) => {
             if (error) {
@@ -62,16 +65,104 @@ app.post('/', (req, res) => {
                     error: 'Error al cargar tarea'
                 });
             }
+            res.status(200).json({
+                ok: true,
+                lista: newTermino,
+                mensaje: 'Agregado correctamente'
+            });
         });
-    }
-    List.find({}).populate('list', 'termino').exec((error, lista) => {
-        List.count({}, (error, conteo) => {
-            lst = lista;
-            res.redirect('/');
+
+
+    })
+    //===========================================
+    //        Actualizar lista
+    //===========================================
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+
+
+
+    List.findById(id, (err, lista) => {
+
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario  ' + id,
+                errors: err
+            });
+        }
+
+        if (!lista) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        }
+
+
+        lista.termino = req.body.termino;
+        lista.complete = req.body.complete;
+
+
+        lista.save((err, listaUpdate) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar Lista',
+                    list: req.body,
+                    errors: err
+                });
+            }
+
+            return res.status(200).json({
+                ok: true,
+                usuario: listaUpdate
+
+            });
+
         });
+
+
+
     });
 
-})
+    app.delete('/:id', (req, res) => {
+
+        var id = req.params.id;
+
+        Usuarios.findByIdAndRemove(id, (err, listaBorrada) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al borrar lista',
+                    errors: err
+                });
+            }
+
+            if (!listaBorrada) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'No existe una lista con ese id'
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                lista: listaBorrada
+            });
+
+        });
 
 
+    });
+
+
+
+
+});
 module.exports = app;
